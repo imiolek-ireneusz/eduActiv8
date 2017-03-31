c6de06901469c790f9678f4c3b0a0ffbd33432ff
imiolek-ireneusz <imiolek.i@googlemail.com>
2017-03-31 19:16:08 +0100

    File cleanup and fix to make top panel buttons after finger up.

--- classes/logoimg.py
+++ classes/logoimg.py
@@ -2,25 +2,21 @@
 
 import os
 import pygame
-
 from game_boards import game000
 
 
 class LogoImg(pygame.sprite.Sprite):
-    'holds the logo in top left corner'
+    """holds the logo in top left corner - also used as a home category button"""
 
     def __init__(self, mainloop):
-        # Call the parent's constructor
         pygame.sprite.Sprite.__init__(self)
         self.mainloop = mainloop
         self.state = 2
         self.mouse_over = False
         self.image = pygame.Surface([166, 146])
         self.image.fill((60, 60, 60))
-
         self.rect = self.image.get_rect()
         self.rect.topleft = [0, 0]
-        # self.img = self.image
         self.img_pos = (0, 0)
         try:
             self.img1 = pygame.image.load(os.path.join('res', 'images', "logo_n.png")).convert_alpha()
@@ -29,7 +25,6 @@ class LogoImg(pygame.sprite.Sprite):
         except:
             pass
         self.update()
-        # self.image.set_colorkey((255,75,0))
 
     def handle(self, event):
         if event.type == pygame.MOUSEMOTION:
@@ -38,14 +33,11 @@ class LogoImg(pygame.sprite.Sprite):
                 self.on_mouse_over()
             else:
                 self.deselect_all()
+                self.mainloop.info.buttons_restore()
+                self.mainloop.info.reset_titles()
 
         elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             if not (self.mainloop.m.ldrag or self.mainloop.m.rdrag):
-
-                # for each in self.games_current:
-                #    each.state = 0
-                # self.games_current[row].state = 2
-
                 if self.mainloop.m.active_o is not None:
                     self.mainloop.m.active_o.state = 0
                     self.mainloop.m.active_o = None
@@ -54,14 +46,10 @@ class LogoImg(pygame.sprite.Sprite):
                     self.mainloop.m.active_cat_o = None
                 if self.state < 2:
                     self.mainloop.sfx.play(4)
-
                 self.state = 2
-
                 self.mainloop.m.active_cat = 0
-                # self.tab_l_scroll = (self.scroll_l // self.scroll_step)
-
-                self.mainloop.m.game_constructor = game000.Board  # self.games_current[row].game_constructor
-                self.mainloop.m.game_variant = 0  # self.games_current[row].variant
+                self.mainloop.m.game_constructor = game000.Board
+                self.mainloop.m.game_variant = 0
                 self.mainloop.m.active_game_id = 0
                 self.mainloop.m.game_started_id = -1
                 self.mainloop.m.tab_game_id = -5
@@ -87,10 +75,8 @@ class LogoImg(pygame.sprite.Sprite):
         self.deselect_all()
         if self.state != 2:
             self.state = 1
-
         self.mouse_over = True
         self.update()
-        # print("enter (in logoimg.py)")
         self.mainloop.redraw_needed[2] = True
 
     def on_mouse_out(self):
@@ -99,7 +85,6 @@ class LogoImg(pygame.sprite.Sprite):
             if self.state != 2:
                 self.state = 0
             self.update()
-            # print("out logoimg.py")
             self.mainloop.redraw_needed[2] = True
 
     def update(self):
