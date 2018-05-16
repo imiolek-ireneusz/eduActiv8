b0dd60e9ba7b13afdd3319d5e4982905f211ec73
imiolek-ireneusz <imiolek.i@googlemail.com>
2018-05-16 11:03:01 +0100

    Minor fixes to symmetry activities.

--- game_boards/game099.py
+++ game_boards/game099.py
@@ -26,29 +26,22 @@ class Board(gd.BoardGame):
         self.board.draw_grid = False
         self.brush_size = 2
         self.prev_snap = None
-
         self.correct = False
-
         self.color_s = random.randrange(50, 90, 5)
         self.color_v = random.randrange(230, 255, 5)
 
         h = random.randrange(0, 255, 1)
         self.bg_color = [255, 255, 255]
         color = [255, 255, 255]
-        white = (255, 255, 255)
         self.transp = (0, 0, 0, 0)
 
         self.active_color = ex.hsva_to_rgba(h, 127, 255, 127)
         self.border_color = ex.hsv_to_rgb(h, 200, 205)
-
         self.lbl_font_color = ex.hsv_to_rgb(17, 255, 175)
         self.guides_color = [200, 200, 200]
         self.axis_color = [255, 0, 0]
-        scheme = "white"
         if self.mainloop.scheme is not None:
             if self.mainloop.scheme.dark:
-                scheme = "black"
-                white = (0, 0, 0)
                 self.bg_color = (0, 0, 0)
                 color = (0, 0, 0)
                 self.guides_color = (30, 30, 30)
@@ -58,26 +51,16 @@ class Board(gd.BoardGame):
         self.chapters = self.mainloop.xml_conn.get_chapters(self.mainloop.m.game_dbid,
                                                             self.mainloop.config.user_age_group)
 
-
-        #data = [27, 20, random.randint(3, 5), random.randint(0, 3)]
-        #data = [27, 20, random.randint(3, 5), random.randint(0, 3), True]
-
-        #[19, 14, random.randint(3, 5), random.randint(0, 3), half]
         if lvl_data[2] == 0:
             data = [18, 14, random.randint(3, 5), random.randint(0, 3), lvl_data[-1]]
         else:
             data = lvl_data
 
         self.data = data
-
         self.max_points = self.data[2]
         self.direction = self.data[3]
         self.half_only = self.data[4]
 
-        #self.images = ["ax_h.png", "ax_v.png", "ax_d1.png", "ax_d2.png"]
-
-        #self.mainloop.info.hide_buttons(0, 0, 0, 0, 1, 1, 1, 0, 0)
-        #self.vis_buttons = [0, 0, 0, 0, 1, 1, 1, 0, 0]
         self.vis_buttons = [0, 1, 1, 1, 1, 0, 1, 0, 1]
         self.mainloop.info.hide_buttonsa(self.vis_buttons)
 
@@ -93,9 +76,10 @@ class Board(gd.BoardGame):
         else:
             self.grid_density_div = 1
 
-        self.guide_scale = self.board.scale# // self.grid_density_div
-        self.left_padding = 2#3
+        self.guide_scale = self.board.scale
+        self.left_padding = 2
         self.px_padding = self.left_padding * scale + self.layout.game_left
+
         # canvas
         self.canvas_side = min(data[0] - self.left_padding, data[1])
         self.canvas_half_size = self.canvas_side // 2
@@ -104,15 +88,15 @@ class Board(gd.BoardGame):
         self.canvas_block = self.board.ships[-1]
         self.canvas_block.set_outline([0, 54, 229], 1)
 
-        #self.board.add_unit(0, 0, 2, 2, classes.board.ImgShip, "", white, os.path.join("symmetry", self.images[self.direction]), 0, alpha=True)
-
-        self.board.add_unit(data[1]+self.left_padding, 0, 2, 2, classes.board.Letter, "", white, "", 0)
+        self.board.add_unit(data[1]+self.left_padding, 0, 2, 2,
+                            classes.board.Letter, "", self.transp, "", 0, alpha=True)
         self.check_btn = self.board.ships[-1]
         self.check_btn.checkable = True
         self.check_btn.init_check_images(align=1, shrink=1)
         self.btn_down = False
 
-        self.board.add_unit(data[1]+self.left_padding, 6, 2, 2, classes.board.ImgCenteredShip, "", white, img_src='nav_refreshd.png', alpha=True)
+        self.board.add_unit(data[1]+self.left_padding, 6, 2, 2, classes.board.ImgCenteredShip, "",
+                            self.transp, img_src='nav_refreshd.png', alpha=True)
         self.rt_btn = self.board.ships[-1]
 
         for each in self.board.ships:
@@ -128,8 +112,8 @@ class Board(gd.BoardGame):
             pygame.SRCALPHA)
 
         self.new_screen()
-
         self.reset()
+
         if self.mainloop.m.game_variant == 0:
             self.random_points = self.pick_half_shape()
         elif self.mainloop.m.game_variant == 1:
@@ -178,9 +162,6 @@ class Board(gd.BoardGame):
             pygame.draw.polygon(self.layer, self.border_color, p2, 3)
             self.layer_blit()
 
-        #draw the task shape outline
-        #pygame.draw.polygon(self.layer, self.border_color, self.random_points, 3)
-
         self.draw_axis(self.direction)
 
         self.reset()
@@ -217,9 +198,6 @@ class Board(gd.BoardGame):
             self.check_btn.set_display_check(False)
             self.correct = False
 
-        #if self.correct:
-        #pygame.time.delay(1000)
-        #self.level.next_board()
         if not self.correct:
             if self.rt_btn.img_src != "nav_refresh.png":
                 self.rt_btn.change_image("nav_refresh.png")
@@ -344,15 +322,18 @@ class Board(gd.BoardGame):
         if self.direction == 0:
             # add 2 points on a line and one outside
             while inner:
-                p1 = [random.randrange(1, self.canvas_side - 1) * self.guide_scale, (self.canvas_side // 2) * self.guide_scale]
-                p2 = [random.randrange(1, self.canvas_side - 1) * self.guide_scale, (self.canvas_side // 2) * self.guide_scale]
+                p1 = [random.randrange(1, self.canvas_side - 1) * self.guide_scale,
+                      (self.canvas_side // 2) * self.guide_scale]
+                p2 = [random.randrange(1, self.canvas_side - 1) * self.guide_scale,
+                      (self.canvas_side // 2) * self.guide_scale]
                 if abs(p1[0] - p2[0]) > 2 * self.guide_scale:
                     inner = False
             points.append(p1)
 
             # add remaining points
             while n < self.max_points - 2:
-                pn = [random.randrange(1, self.canvas_side - 1) * self.guide_scale, random.randrange(1, self.canvas_side // 2 - 2) * self.guide_scale]
+                pn = [random.randrange(1, self.canvas_side - 1) * self.guide_scale,
+                      random.randrange(1, self.canvas_side // 2 - 2) * self.guide_scale]
                 if pn not in points:
                     points.append(pn)
                     n += 1
@@ -361,15 +342,18 @@ class Board(gd.BoardGame):
         elif self.direction == 1:
             # add 2 points on a line and one outside
             while inner:
-                p1 = [(self.canvas_side // 2) * self.guide_scale, random.randrange(1, self.canvas_side - 1) * self.guide_scale]
-                p2 = [(self.canvas_side // 2) * self.guide_scale, random.randrange(1, self.canvas_side - 1) * self.guide_scale]
+                p1 = [(self.canvas_side // 2) * self.guide_scale,
+                      random.randrange(1, self.canvas_side - 1) * self.guide_scale]
+                p2 = [(self.canvas_side // 2) * self.guide_scale,
+                      random.randrange(1, self.canvas_side - 1) * self.guide_scale]
                 if abs(p1[1] - p2[1]) > 2 * self.guide_scale:
                     inner = False
             points.append(p1)
 
             # add remaining points
             while n < self.max_points - 2:
-                pn = [random.randrange(1, self.canvas_side // 2 - 2) * self.guide_scale, random.randrange(1, self.canvas_side - 1) * self.guide_scale]
+                pn = [random.randrange(1, self.canvas_side // 2 - 2) * self.guide_scale,
+                      random.randrange(1, self.canvas_side - 1) * self.guide_scale]
                 if pn not in points:
                     points.append(pn)
                     n += 1
@@ -445,7 +429,7 @@ class Board(gd.BoardGame):
         # redraw outlines
         pygame.draw.polygon(self.layer, self.border_color, points, 3)
         self.layer_blit()
-        #self.draw_guides(self.guide_scale)
+
         self.draw_axis(self.direction)
         self.reset()
         self.copy_to_screen()
@@ -483,7 +467,8 @@ class Board(gd.BoardGame):
         l = len(p)
         b = False
         for i in range(l):
-            b = b or (ccw(p[i-0], p[i-2], p[i-3]) != ccw(p[i-1], p[i-2], p[i-3]) and ccw(p[i-0], p[i-1], p[i-2]) != ccw(p[i-0], p[i-1], p[i-3]))
+            b = b or (ccw(p[i-0], p[i-2], p[i-3]) != ccw(p[i-1], p[i-2], p[i-3]) and
+                      ccw(p[i-0], p[i-1], p[i-2]) != ccw(p[i-0], p[i-1], p[i-3]))
         return b
 
     def collinear(self, p0, p1, p2):
@@ -614,16 +599,12 @@ class Board(gd.BoardGame):
         self.max_points = tool
         self.reset()
         self.mainloop.redraw_needed[0] = True
-        #if tool < 4:
-        #    self.tool_door.set_pos(self.board.active_ship_pos)
 
     def change_axis(self, axis):
         self.direction = axis
         self.new_screen()
         self.reset()
         self.mainloop.redraw_needed[0] = True
-        #if axis < 7:
-        #    self.tool_door.set_pos(self.board.active_ship_pos)
 
     def paint_line(self, state):
         if state == 0:
@@ -675,6 +656,7 @@ class Board(gd.BoardGame):
                 x2 = x1
                 y2 = y1
             points = []
+
             points.append([int(p1[0] - x1), int(p1[1] + y1)])
             points.append([int(p1[0] + x1), int(p1[1] - y1)])
 
@@ -699,7 +681,7 @@ class Board(gd.BoardGame):
 
     def update(self, game):
         game.fill((255, 255, 255))
-        gd.BoardGame.update(self, game)  # rest of painting done by parent
+        gd.BoardGame.update(self, game)
 
     def v2_to_int(self, vector):
         integers = [int(each) for each in vector]
