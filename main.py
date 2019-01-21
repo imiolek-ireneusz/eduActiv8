f1a9b7bab696b2d7101b64ab42dd4b24afa12191
imiolek-ireneusz <imiolek.i@googlemail.com>
2019-01-21 12:41:50 +0000

    Revert "Separate launchers created to run the program in either python2 or python3. Also some files used in Android build added. Ref. #25."
    This reverts commit 983ca154d996d6125c20c2dc2c5f75aa4786a25b.

--- main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python2
-# Application starter for use with Python 2
-
-import eduactiv8
-
-if __name__ == "__main__":
-    eduactiv8.main()
--- main3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# Application starter for use with Python 3
-
-import eduactiv8
-
-if __name__ == "__main__":
-    eduactiv8.main()
